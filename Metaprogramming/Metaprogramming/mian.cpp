94d995b2883dd1e54cfa252f62bbb4567eb155fe
Egor Yurchenko <egorkay01@gmail.com>
2024-05-18 11:52:02 +0300

    Add print_ip overload for std::tuple

--- Metaprogramming/Metaprogramming/mian.cpp
+++ Metaprogramming/Metaprogramming/mian.cpp
@@ -23,8 +23,8 @@ void print_ip(T value) {
 }
 
 template<bool = true>
-void print_ip(std::string string) {
-	std::cout << string << std::endl;
+void print_ip(std::string str) {
+	std::cout << str << std::endl;
 }
 
 template<typename T>
@@ -37,7 +37,7 @@ template<Iterable T>
 struct is_iterable: std::true_type {};
 
 template<typename T, std::enable_if_t<is_iterable<T>::value, bool> = true>
-void print_ip(const T& value) {
+void print_ip(T value) {
 	auto it = value.begin();
 	for (size_t i = value.size(); i > 0; --i, it++) {
 		std::cout << *it;
@@ -49,10 +49,37 @@ void print_ip(const T& value) {
 }
 
 
+namespace details {
+
+	template<size_t ind, class Tuple>
+	void output_values(Tuple& args) {
+		std::cout << "\n";
+	}
+
+	template<size_t ind, class Tuple, typename Head, typename... Args>
+	void output_values(Tuple& args) {
+
+		std::cout << std::get<ind>(args);
+		
+		if constexpr (std::tuple_size_v<Tuple>-1 != ind) {
+			std::cout << ".";
+		} 
+
+		output_values<ind + 1, Tuple, Args...>(args);
+	}
+
+}
+
+template<typename Head, typename... Args>
+void print_ip(std::tuple<Head, Args...> args) {
+	static_assert((std::is_same_v<Head, Args> && ...), "Tuple must have the same types");
+
+	details::output_values<0, decltype(args), Head, Args...>(args);
+}
+
+
 int main()
 {
-
-	print_ip(2); // 255 
 	print_ip(int8_t{ -1 }); // 255 
 	print_ip(int16_t{ 0 }); // 0.0 
 	print_ip(int32_t{ 2130706433 }); // 127.0.0.1 
@@ -60,7 +87,7 @@ int main()
 	print_ip(std::string{ "Hello, World!"}); // Hello, World! 
 	print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400 
 	print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100 
-	//print_ip(std::make_tuple(123, 456, 789, 0)); // 123.456.789.0 
+	print_ip(std::make_tuple(123, 456, 789, 0)); // 123.456.789.0 
 
 	return 0;
 }
